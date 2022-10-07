5f78b25f542188d31a0c3d4704cb1716dd8d7e18
Amangggganteng <103506892+Amangggganteng@users.noreply.github.com>
2022-10-07 15:36:34 +0700

    Update generate.py

--- StringGeneratorBot/generate.py
+++ StringGeneratorBot/generate.py
@@ -32,7 +32,7 @@ from telethon.errors import (
 )
 
 
-ask_ques = "**» ᴩʟᴇᴀsᴇ ᴄʜᴏᴏsᴇ ᴛʜᴇ ᴩʏᴛʜᴏɴ ʟɪʙʀᴀʀʏ ғᴏʀ ᴡʜɪᴄʜ ʏᴏᴜ ᴡᴀɴᴛ ᴛᴏ ɢᴇɴᴇʀᴀᴛᴇ sᴛʀɪɴɢ :**"
+ask_ques = "**» Pilih String session yang ingin anda buat :**"
 buttons_ques = [
     [
         InlineKeyboardButton("ᴩʏʀᴏɢʀᴀᴍ", callback_data="pyrogram1"),
